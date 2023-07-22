acf682eb787fce6c07070d3cda6982658c4b8c1f
Teasotea <sofi16616@gmail.com>
2023-07-22 02:09:03 -0400

    major updates

---
+++ prompt_lib.py
@@ -0,0 +1,111 @@
+
+STARTER_PROMPT = """
+You are a ScriptWriter for YouTube and SnapChat Bloggers. 
+You have experience in building a story around a given topic. 
+
+INITIAL RULES FOR SCRIPTS: 
+a) Use words for dates: 1970s = nineteen-seventies. 
+b) Don't use dollar sign: for instance $20 = 20 dollars OR $20 bond = twenty-dollar bond 
+c) Use "to" instead of hyphen: 2-10 people = 2 to 10 people 
+d) Don't use slashes: 20/20 = twenty-twenty 
+e) Any symbol should be properly spelt out: 900km/h = 900 kilometers per hour 
+f) Number should be read digit-by-digit: 911 = 9-1-1 OR Boeing 707 = Boeing seven-oh-seven 
+g) Put a full-stop after each header and leave one paragraph space above and below. 
+
+CRITERIA OF THE BEST SCRIPT: 
+Don't make up things. 
+Avoid Unnecessary repetition of certain events or plot points in a story.
+Don't add info that don’t align with the topic or perceived narrative of the story.
+Don't give advice or opinions. Keep it almost strictly a story. 
+The story should be educational and contain facts. 
+Add as many details as possible. 
+Avoid using summ-up words like 'additionally', 'in this section', 'conclution' and so on.
+Keep using storytelling tone.
+Don't introduce yourself as an AI bot, just write the story. 
+You should mention names, events, etc. that will guide the story.
+
+HISTORY:
+{input}, {history}
+"""
+
+# _____________________________ STORY PROMPTS _____________________________
+
+intro_story= '''
+A word is a group of letters separated by spaces, or the group of letters that starts or ends a sentence.
+Now, generate ONLY 80-150 words intro, containing the keywords from the topic sentence coherently present in it (not forced). 
+Don't conclude the story, don't prepare epilogue. Don't add items, this is intro and shouldn't spoil the future story.
+'''
+
+backstory_story = '''
+A word is a group of letters separated by spaces, or the group of letters that starts or ends a sentence. Generate 800-1000 words first part of full story, breaking down the timeline of things.
+
+If there's enough content, just write the story as a timeline of the events. If there's not enough content, write the story using “narratives”.
+This involves making use of the central theme, or idea, or stereotype, or expected presuppositions of the audience to write the story.
+A story with the narrative starts with Backstory: Introduce the topic and the main event in the story. Do not keep main event from the viewers. 
+We need to introduce them event early in a way that will hook them and buttress the narrative that we've come up with for the story. 
+We need to convince the viewers that it is what they should care about. 
+Each time we move to a different topic, make transitions into the next sections with this narrative in mind.
+
+Don't conclude the story, don't prepare epilogue, don't give advice or opinions. Keep it almost strictly a story.
+'''
+
+middlepart_story = '''
+A word is a group of letters separated by spaces, or the group of letters that starts or ends a sentence.
+Generate 800-1000 words next part of full story.
+Middle: Here, discuss every other thing we can relate to the story and/or the narrative. 
+Tell users about: similar cases like the one we’re discussing that explains why what we’re discussing is important, 
+other events in our own story that are a continuation of the events that have occurred.
+
+Don't conclude the story, don't prepare epilogue, don't give advice or opinions. Keep it almost strictly a story.
+'''
+
+epilogue_story = '''
+A word is a group of letters separated by spaces, or the group o4,705 words 31,032 characters
+f letters that starts or ends a sentence. 
+Generate 800-1000 words last part of full story.
+Everything must be a story, 
+so it should be a continued story on the main events of the topic, the conclusion of the story, or another story that fits with the narrative. 
+
+Don't give advice or opinions. Keep it almost strictly a story. Don't make conclusion, don't prepare epilogue, don't include your point of view.
+'''
+
+# _____________________________ LIST PROMPTS _____________________________
+
+intro_list= '''
+A word is a group of letters separated by spaces, or the group of letters that starts or ends a sentence.
+Now, generate ONLY 80-200 words intro, containing the keywords from the topic sentence coherently present in it (not forced). 
+Don't conclude the story, don't prepare epilogue.
+'''
+
+preparelist_list = lambda topX, topic: f'''
+Prepare list of top {str(topX)} {topic}.. Strictly follow the format [item1, item2, item3, ..., itemX]. It should be a list .
+Use '[' and ']' tokens, make it easy to parse with python code. don't use extra words and don't make up facts. 
+Items should fully align with the topic
+'''
+
+item_list = lambda topic, topX, item_num: f'''
+A word is a group of letters separated by spaces, or the group of letters that starts or ends a sentence.
+Generate {int(5000/topX)}-{int(4000/topX)} words story on {topic} about next item (number {item_num}) from items, mensioned above.
+Concentrate on details. Don't use non-meaningful words. The story must be fascinating ad fairy tail, but at the same time it should be educational and contain facts.
+Don't make up things.
+Don't conclude the story, don't prepare epilogue, don't give advice or opinions. Keep it almost strictly a story.
+'''
+
+epilogue_list = lambda topic: f'''
+A word is a group of letters separated by spaces, or the group of letters that starts or ends a sentence. 
+Generate 200 words conclusion of the story on {topic} about items, mensioned above, so it should be a continued story on the main events of the topic, 
+the conclusion of the story, or another story that fits with the narrative. 
+
+Don't give advice or opinions. Keep it almost strictly a story. Don't use non-meaningful words, but rather concentrate on details.
+'''
+
+WORD_COUNT = 5000
+PART_COUNT = int(WORD_COUNT/600)
+
+STORY_YT_PROMPT = [intro_story, backstory_story, *[middlepart_story]*8, epilogue_story] #*m_part*(PART_COUNT-2)
+LIST_YT_PROMPT = lambda topX, topic: [intro_list, preparelist_list(topX, topic), 
+                                      *[item_list(topic, topX, p+1) for p in range(topX)], 
+                                      epilogue_list(topic)]
+STORY_SC_PROMPT = []
+LIST_SC_PROMPT = []
+
--- script_bot.py
+++ script_bot.py
@@ -1,93 +1,30 @@
+# from nltk.tokenize import word_tokenize
+# import nltk
+# nltk.download('punkt')
+
 from dotenv import load_dotenv
 import os
 import streamlit as st
 from streamlit_chat import message
 
-from langchain import OpenAI, PromptTemplate
+from langchain import PromptTemplate
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
 
-template = """
-CONTEXT:
-You are a ScriptWriter for YouTube and SnapChat Bloggers. You have experience in building a story around a given topic.
+from prompt_lib import STARTER_PROMPT, STORY_YT_PROMPT, LIST_YT_PROMPT, STORY_SC_PROMPT, LIST_SC_PROMPT
+from langchain.schema import (SystemMessage, HumanMessage)
 
-GOAL:
-You will become my scriptwriter today. You need to help me to prepare a story for the video.
-
-INITIAL RULES FOR SCRIPTS:
-a) Use words for dates: 1970s = nineteen-seventies. 
-b) Don't use dollar sign: for instance $20 = 20 dollars OR $20 bond = twenty-dollar bond 
-c) Use "to" instead of hypen: 2-10 people = 2 to 10 people 
-d) Don't use slashes: 20/20 = twenty-twenty
-e) Any symbol should be properly spelt out: 900km/h = 900 kilometers per hour 
-f) Number should be read digit-by-digit: 911 = 9-1-1 OR Boeing 707 = Boeing seven-oh-seven 
-g) Put a full-stop after each header and leave one paragraph space above and below.
-
-CRITERIA OF THE BEST SCRIPT:
-Don't make up things. 
-Avoid Unnecessary repetition of certain events or plot points in a story
-Don't add info that don’t align with the topic or perceived narrative of the story 
-Don't give advice or opinions. Keep it almost strictly story. 
-The story should be educational and contain facts. 
-Add as many details as possible.
-Avoid using buzzwords like 'additionally', 'in this section' and so on. 
-Don't repeat names of characters. 
-Don't introduce yourself as AI bot, just write the story.
-Don't use the word ENTRY in the list script.
-
-STRUCTURE OF SCRIPTWRITING SESSION:
-1. I will set the topic and social media: YouTube or SnapChat
-2. I will specify the type of script - story script or list script
-3. You will return a script
-4. I will check the content and may ask you to regenerate som information
-5. You will regenerate more information if needed
-
-FORMAT OF OUR INTERACTION
-— I will let you know when we can proceed to the next step. Don't go there without my instructions.
-— You will rely on the context of this script writing session at every step.
-
-INFORMATION ABOUT SCRIPT TYPES AND SOCIAL MEDIA:
-YouTube List Script:
-A total word count of 4000 - 5000 words.
-Strat from an 80-200 words intro, containing the keywords from the topic sentence coherently present in it (not forced).
-X number of entries/sections with a similar word count range. For example, for topic “X Deadliest Gangs in the US”, if we find 10 entries (X=10), then each section has to have between 400 - 600 words to match the 5,000-total word count requirement. If we find 20 entries (X=20), then each section has to have between 200 - 300 words to match the 5,000-total word count requirement.
-
-YouTube Story Script:
-A total word count of 5000 words.
-Strat from an 80-200 words intro, containing the keywords from the topic sentence coherently present in it (not forced), then a full story (with sections if necessary) breaking down the timeline of things. 
-A full story (with sections if necessary) breaking down the timeline of things. 
-X number of entries/sections with a similar word count range (each section highlights a new idea, act, timeline, or plot point, in the story that relates back to and occasionally mentions the central theme and narrative present in the topic)
-If there’s enough content, just write the story as a timeline of the events. If there’s not enough content, write the story using “narratives”. This involves making use of the central theme, or idea, or stereotype, or expected presuppositions of the audience to write the story. A story with the narrative combines different parts: 1. Backstory: Introduce the topic and the main event in the story. Do not keep main event from the viewers. We need to introduce them event early in a way that will hook them and buttress the narrative that we’ve come up with for the story. We need to convince the viewers that it is what they should care about. Each time we move to a different topic, make transitions into the next sections with this narrative in mind. 2. Middle: Here, discuss every other thing we can relate to the story and/or the narrative. Tell users about: similar cases like the one we’re discussing that explains why what we’re discussing is important, other events in our own story that are a continuation of the events that have occurred. 3. End: Conclude with a section reprising the narrative. Everything must be a story, so it should be a continued story on the main events of the topic, the conclusion of the story, or another story that fits with the narrative. 
-3. SnapChat List Script:
-a) A total word count of 680 - 750 words.
-b) Strat from a 20 words intro, containing the keywords from the topic sentence coherently present in it (not forced)
-c) Prepare 12 - 16 entries/sections with a word count range of 45 - 55 words each.
-
-4. SnapChat Story Script:
-a) A total word count of 680 - 750 words.
-b) Strat from a 20 words intro, containing the keywords from the topic sentence coherently present in it (not forced)
-c) Prepare a full story (with sections if necessary) breaking down the timeline of things.
-
-HISTORY:
-{input}, {history}
-"""
-
-
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage
-)
+SOCIAL_MEDIA = ['YouTube', 'SnapChat']
+SCRIPT_TYPE = ['Story Script', 'List Script']
 
 def init():
     load_dotenv()
-
     if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
         print("OPENAI_API_KEY is not set")
         exit(1)
     else:
         print("OPENAI_API_KEY is set")
-
     st.set_page_config(
         page_title="ScriptWriter Bot",
         page_icon="🖋🎥"
@@ -95,37 +32,48 @@ def init():
 
 def main():
     init()
-
     chat = ChatOpenAI(
         temperature=0.1,	
-        model_name="gpt-3.5-turbo"
+        model_name = "gpt-3.5-turbo"
         )
-
     if "messages" not in st.session_state:
         st.session_state.messages = [
-            SystemMessage(input_variables=["input", "history"], content=template)
+            SystemMessage(input_variables=["input", "history"], content=STARTER_PROMPT)
             ]
-
+        
     st.header("ScriptWriter Bot 🖋🎥")
-
-    PROMPT = PromptTemplate(template=template, input_variables=["input", 'history'])
-
+    PROMPT = PromptTemplate(template=STARTER_PROMPT , input_variables=["input", 'history'])
     conversation = ConversationChain(prompt=PROMPT, llm=chat, memory=ConversationBufferMemory(ai_prefix="ScriptWriter", human_prefix="MainEditor"), verbose=True)
-
+    short_conversation = ConversationChain(prompt=PROMPT, llm=chat, verbose=True)
+    
+     # generate a function  that should do try exept and return error message if the response is too long 
     def generate_response(prompt):
-        pred = conversation.predict(input=prompt)
-        return pred
-
+        try:
+            return conversation.predict(input=prompt)
+        except:
+            print('Conversation is too long. Trying to generate response anyway.')
+            return short_conversation.predict(input=prompt)
+        
+        # if len(conversation.predict(input=prompt)) <= 1500:
+        #     pred = conversation.predict(input=prompt)
+        # else:
+        #     short_conversation = ConversationChain(prompt=prompt, llm=chat, verbose=True)
+            
     with st.sidebar:
         st.header("🖋 Script Details")
         st.subheader("Specify below the topic, type of script, and social media platform: ")
         user_input = st.text_input("Enter the topic: ", key="user_input")
-        script_type = st.sidebar.selectbox("Select the type of script", [None, "List Script", "Story Script"])
-        platform = st.sidebar.selectbox("Select platform", [None, "YouTube", "SnapChat"])
+        script_type = st.selectbox("Select the type of script", [None, "List Script", "Story Script"])
+        
+        if script_type == "List Script":
+            topX = st.text_input("Enter number of items in the list (Top X..), X =", key = "top X")
+            if topX: topX = int(topX)
+            
+        platform = st.selectbox("Select platform", [None, "YouTube", "SnapChat"])
         
         is_generate_script = st.button('Generate script')
         if is_generate_script and not user_input:
-            st.error("Please, enter the topic and click the button to generate script.")
+            st.error("Click the \"Generate script\" button to generate script.")
             
         if 'generated' not in st.session_state:
             start_writer = 'Hi! I am your ScriptWriter Bot! Let\'s get started!'
@@ -137,24 +85,53 @@ def main():
         is_edit_mode = st.button('Edit script')
         if is_edit_mode and not user_input:
             st.error("Nothing to edit. Please, enter the topic and click the button to generate script.")
-
-        if user_input:
-            if script_type != None and platform != None and is_generate_script:
-                st.session_state.messages.append(HumanMessage(content='topic:' + user_input + ', type of script: ' + script_type + ', platform: ' + platform))
-                with st.spinner("Please, wait. I am in the process of creating script for you..."):
-                    response = generate_response(user_input)
-                st.session_state.past.append(user_input)
-                st.session_state.generated.append(response)
-            elif not is_generate_script and not is_edit_mode:
-                st.error("Please, select the type of script and platform and click the button to generate script.")
-
+        
         st.write('Click the button to save the script to Google Docs.')
         is_save_script = st.button('Save script to Google Docs')
         if is_save_script and is_generate_script or is_edit_mode:
             st.write('Script will be uploaded.')
         elif is_save_script and not is_generate_script and not is_edit_mode:
             st.error("Nothing to save. Please, enter the topic and click the button to generate script.")
+
+    if user_input and script_type and platform:
+        
+        if script_type == SCRIPT_TYPE[0] and platform == SOCIAL_MEDIA[0]:
+            PROMPT_ORDER = STORY_YT_PROMPT
+        elif script_type == SCRIPT_TYPE[0] and platform == SOCIAL_MEDIA[1]:
+            PROMPT_ORDER = STORY_SC_PROMPT
+        elif script_type == SCRIPT_TYPE[1] and platform == SOCIAL_MEDIA[0]:
+            PROMPT_ORDER = LIST_YT_PROMPT(topX, user_input)
+        elif script_type == SCRIPT_TYPE[1] and platform == SOCIAL_MEDIA[1]:
+            PROMPT_ORDER = LIST_SC_PROMPT(topX, user_input) 
+        
+        topXitems = []
+        print(topX, script_type, platform, PROMPT_ORDER)
             
+        if script_type != None and platform != None and is_generate_script:
+            st.session_state.messages.append(HumanMessage(content='topic:' + user_input))
+            st.session_state.past.append(user_input)
+            resulting_response = []
+            for i, prompt in enumerate(PROMPT_ORDER):
+                with st.spinner("Please, wait. I am in the process of creating script for you..."):
+                    if topX:
+                        if topXitems == []:
+                            final_prompt = 'SESSION INFO: topic - Top ' + str(topX)+ ' ' + user_input + ' ' + prompt
+                        else:
+                            final_prompt = 'SESSION INFO: topic - Top ' + ', items are: ' + str(topX)+ str(topXitems) + ' ' + user_input + ' ' + prompt 
+                    else:
+                        final_prompt = 'SESSION INFO: topic - ' + user_input + ' ' + prompt
+                    response = generate_response(final_prompt) 
+                    if i == 1 and topX:
+                        topXitems = response #.split('[')[1].split(']')[0].split(',')
+                        resulting_response.append('RESPONSE #' + str(i+1) + ': ' + response)
+                    else:
+                        resulting_response.append('RESPONSE #' + str(i+1) + ': ' + response)
+            resulting_response = ' '.join(resulting_response)
+            st.session_state.generated.append(resulting_response)
+                
+        elif not is_generate_script and not is_edit_mode:
+            st.error("Click the \"Generate script\" button to generate script.")
+         
     
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])-1, -1, -1):
